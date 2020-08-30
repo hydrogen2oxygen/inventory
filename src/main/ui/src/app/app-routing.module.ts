493ad1f7ab89125aad2fdf7a016c9abc23fe1155
PeterLuxus <luxuspeter@googlemail.com>
2020-08-30 20:26:19 +0200

    DataBaseService

--- src/main/ui/src/app/app-routing.module.ts
+++ src/main/ui/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {InventoryComponent} from "./components/inventory/inventory.component";
+import {ItemComponent} from "./components/item/item.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: InventoryComponent},
+  {path:'home', redirectTo: '', pathMatch: 'full'},
+  {path:'item', component: ItemComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/main/ui/src/app/app.module.ts
+++ src/main/ui/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { InventoryComponent } from './components/inventory/inventory.component';
+import { ItemComponent } from './components/item/item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    InventoryComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
